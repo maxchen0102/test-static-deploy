969fdcf575db646ad24c39c5178c3788a8974200
Chris.Chen <Chris.Chen@splashtop.com>
2024-08-08 14:00:50 +0800

    1

---
+++ scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+
+    // 打開新視窗
+    $('#btn-open-window').click(function() {
+        window.open('https://www.google.com');
+        alert('Button clicked!');
+    });
+
+
+});
